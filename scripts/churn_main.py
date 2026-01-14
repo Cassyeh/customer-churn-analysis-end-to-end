4cd37f386172a256d990063166f664fedf5eda09
Cassyeh <cassandraijezie@gmail.com>
2026-01-14 19:34:28 +0100

    Created eda functions for Phone Services against Churned Customers

--- scripts/churn_main.py
+++ scripts/churn_main.py
@@ -7,7 +7,7 @@ sys.path.append(str(PROJECT_ROOT))
 
 from src.utils import download_file_from_google_drive, load_data_csv, save_data
 from data_cleaning import clean_data, convert_yes_no_columns, impute_zero_tenure_values
-from exploratory_analysis import plot_churn_counts
+from exploratory_analysis import plot_churn_counts, plot_service_vs_churn
 
 file_id = "1763OlxZ9Fun9-x3GYi6BUu_7ot9AfEkJ"   # replace with  file ID
 destination = "./data/raw/telco_customer_churn_data.csv"  # local file name
@@ -15,16 +15,22 @@ destination = "./data/raw/telco_customer_churn_data.csv"  # local file name
 #download_file_from_google_drive(file_id, destination)
 
 # Paths
-RAW_DATA_PATH = "./data/raw/telco_customer_churn_data.csv"
+RAW_DATA_PATH = destination
 PROCESSED_DATA_PATH = "./data/processed/telco_customer_churn_data_cleaned.csv"
 
+# Load Data
 raw_df = load_data_csv(RAW_DATA_PATH)
 
+# Transform Data
 clean_df = clean_data(raw_df, "Churn")
 zero_one_bool_df = convert_yes_no_columns(clean_df)
 filled_total_charges_df = impute_zero_tenure_values(zero_one_bool_df, "TotalCharges")
 
 #save_data(filled_total_charges_df, PROCESSED_DATA_PATH)
 
+#EDA
 churn_visual_path = './visuals/eda/churn_count_eval.png'
 plot_churn_counts(filled_total_charges_df, 'Churn', churn_visual_path)
+
+phone_service_visual_path = './visuals/eda/phone_service_churned_eval.png'
+plot_service_vs_churn(filled_total_charges_df, service_col="PhoneService",title="Phone Service vs Churn Rate",save_path=phone_service_visual_path)
--- scripts/exploratory_analysis.py
+++ scripts/exploratory_analysis.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -20,7 +21,7 @@ def plot_churn_counts(df, target_col='Churn', save_path='visuals/eda/churn_count
     # Add grid
     plt.grid(axis='y', color='gray', linestyle='--', linewidth=1, alpha=0.7)
 
-    ax = sns.countplot(x='Churn', data=df, palette='Set2')
+    ax = sns.countplot(x=target_col, data=df, palette='Set2')
 
     # --- Annotate counts on top of bars ---
     for p in ax.patches:
@@ -40,3 +41,70 @@ def plot_churn_counts(df, target_col='Churn', save_path='visuals/eda/churn_count
     plt.savefig(save_path, dpi=500, bbox_inches='tight')
     
     print(f"{target_col} Plot saved to {save_path}")
+
+
+# Hierarchical service dependencies:
+def plot_service_vs_churn(df, service_col, churn_col="Churn",eligible_condition=None, title=None, save_path='visuals/eda/eval.png'):
+    """
+    Plots churn rate for a given service among eligible customers only.
+    Parameters:
+    df(pd.DataFrame): DataFrame containing the columns
+    service_col(str): Column name for the service column
+    churn_col(str, default 'Churn'): Column name for churn labels
+    eligible_condition(str): The condition that the service meets
+    title(str): Title of the visual
+    """
+
+    data = df.copy()
+
+    # Apply eligibility filter if provided
+    if eligible_condition is not None:
+        data = data.query(eligible_condition)
+
+    total_service_counts = data.groupby(service_col).size()
+    print(total_service_counts)
+    service_churn_total = (
+        data.groupby(service_col)[churn_col].apply(lambda x: (x == 1).sum())
+    ) # x == 1, represents 'Churn' == 'Yes'
+    print(service_churn_total)
+    # Compute churn rate, mean applies avg
+    churn_rate = service_churn_total / total_service_counts
+
+    print(churn_rate)
+
+    #The proportion of customers who churned (1) in that group Churn == 1/total number of customers in service_col
+    churn_service_summary = pd.DataFrame({
+        "TotalCustomers": total_service_counts,
+        "ChurnedCustomers": service_churn_total,
+        "ChurnRate": churn_rate
+    })
+
+    print(churn_service_summary)
+    # --- Plot ---
+    fig, ax1 = plt.subplots(figsize=(8,6))
+
+    # Primary axis: churn rate
+    sns.barplot(x=churn_service_summary.index, y=churn_service_summary["ChurnRate"],palette="Set2",ax=ax1)
+    ax1.set_ylim(0, 1)
+    ax1.set_ylabel("Churn Rate", color="green", fontsize=12)
+    ax1.set_xlabel("Phone Service (0 = No, 1 = Yes)")
+    ax1.set_title(title)
+
+    # Annotate churn rate on bars
+    for i, rate in enumerate(churn_service_summary["ChurnRate"]):
+        ax1.text(i, rate + 0.01, f"{rate:.2%}", ha="center", fontsize=10, color="green")
+
+    # Secondary axis: total customers
+    ax2 = ax1.twinx()
+    sns.lineplot(x=churn_service_summary.index,y=churn_service_summary["ChurnedCustomers"],marker="o",color="blue",linewidth=2,ax=ax2)
+    ax2.set_ylabel("Churned Customers", color="blue", fontsize=12)
+
+    # Annotate total customers
+    for i, count in enumerate(churn_service_summary["ChurnedCustomers"]):
+        ax2.text(i, count + 10, f"{count}", ha="center", fontsize=10, color="blue")
+
+    # --- Create folder if it doesn't exist ---
+    os.makedirs(os.path.dirname(save_path), exist_ok=True) 
+    # --- Save plot ---
+    plt.savefig(save_path, dpi=500, bbox_inches='tight')
+    print(f"{service_col} Plot saved to {save_path}")
